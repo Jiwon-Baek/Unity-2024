546b8a044b73a16d129c57c5d6445430bc9a1524
Jiwon Baek <baekjiwon@snu.ac.kr>
2024-10-09 18:40:26 +0900

    revise to PMSP

--- Machine-Shop/Assets/Block.cs
+++ Machine-Shop/Assets/Block.cs
@@ -7,18 +7,22 @@ using UnityEngine;
 public class Block : MonoBehaviour
 {
     public void Initialize(Color _color, int _blockIndex, 
-        List<Vector3> _positions, Vector3 _source, Vector3 _sink,
-        float[] _moveTime, float[] _finishTime, float[] _startTime)
+        Vector3 _source, Vector3 _position, Vector3 _sink,
+        float[] timetable)
     {
         originalColor = _color;
         blockindex = _blockIndex;
-        positions = _positions;
-        movetime = _moveTime;
-        finishtime = _finishTime;
-        starttime = _startTime;
-        currentPosition = _source;
-        sink = _sink;
 
+        T_created = timetable[0];
+        T_move = timetable[1];
+        T_setup = timetable[2];
+        T_start = timetable[3];
+        T_finish = timetable[4];
+        T_end = timetable[4]+0.5f;
+        currentPosition = _source;
+        source = _source;
+        position = _position;
+        sink = _sink;
     }
 
     Color CreateDarkColor(Color originalColor)
@@ -38,148 +42,116 @@ public class Block : MonoBehaviour
 
         return darkColor;
     }
-
-    public List<Vector3> positions;
+    GameObject block;
     public Vector3 sink;
+    public Vector3 source;
+    public Vector3 position;
+
     public int currentindex = 0; // 앞으로 position이나 time array 의 값을 참조할 때 사용할 index 변수
     public Vector3 currentPosition;     // 현재 위치
     public Vector3 targetPosition;      // 목표 위치
-    public Color processColor = Color.green;  // 진행중일 때의 색상
+    //public Color processColor = Color.green;  // 진행중일 때의 색상
     public Color originalColor;              // 원래 색상
     public Color darkColor;
-    public float movingTime = 0.5f;     // 이동 시간 (0.5초)
-    private float moveProgress = 0.0f;   // 이동 진행 상태 (0.0f ~ 1.0f)
-    private float colorChangeDuration;  // 색상이 변하는 시간
-    private float colorChangeProgress = 0.0f;  // 색상 변경 진행 상태 (0.0f ~ 1.0f)
 
-    public float[] finishtime;
-    public float[] movetime;
-    public float[] starttime;
-    private int num_process;
+    public float movingTime = 2.0f;     // 이동 시간 (0.5초)
+    private float moveProgress = 0.0f;   // 이동 진행 상태 (0.0f ~ 1.0f)
+    // private float colorchangeduration;  // 색상이 변하는 시간
+    // private float colorchangeprogress = 0.0f;  // 색상 변경 진행 상태 (0.0f ~ 1.0f)
+
+    private float T_created;
+    private float T_move;
+    private float T_setup;
+    private float T_start;
+    private float T_finish;
+    private float T_end;
+
     public int blockindex;
-    private float timer = 0.0f;          
-    private float delta;
-    private float target_delta;
+
+    private float timer1 = 0.0f;          
+    private float timer2;
 
     public bool isFinished;
 
     public Renderer blockrenderer;
-    private float eps = 0.2f;
 
     void SetColor(Color _color)
     {
         originalColor = _color;
     }
 
-    //void SetPositions(Vector3[] _positions)
-    //{
-    //    positions = _positions;
-    //}
-
-    //void SetTimeVariables(float[] _movetime, float[] _finishtime, float[] _starttime)
-    //{
-    //    movetime = _movetime;
-    //    finishtime = _finishtime;
-    //    starttime = _starttime;
-    //}
-
     void Start()
     {
         isFinished = false;
         transform.position = currentPosition;
-        positions[0] = transform.position;
-        targetPosition = positions[0];
+        targetPosition = source;
         blockrenderer = GetComponent<Renderer>();
         blockrenderer.material.color = originalColor;
-
         if (blockrenderer == null)
         {
             Debug.LogError("Renderer component not found on the object!");
             return;
         }
-
         darkColor = CreateDarkColor(originalColor);
-
-        num_process = starttime.Length;
-        delta = movetime[0];
-        target_delta = movetime[1] - movetime[0];
-        //target_delta = movetime[0] - 0.0f;
-        // Debug.Log("Block" + blockindex + "has to wait till " + target_delta);
-        SetTarget(positions[0]);
+        SetTarget(position);
 
     }
-    
-    
+
     void Update()
     {
         
-        if (timer >= finishtime[finishtime.Length - 1])
+        if (isFinished)
         {
-            if (isFinished == false)
+            Sink();
+            return;
+        }
+        
+        if (timer1 >= T_created)
+        {
+            if (timer1 >= T_move && timer1 <= T_setup)
             {
+                SetTarget(position);
+                blockrenderer.material.color = darkColor;
+                Move();
+            }
+            else if (timer1 >= T_setup && timer1 <= T_start)
+            {
+                if (transform.position != targetPosition)
+                {
+                    Move();
+                }
+                // Setup
+                blockrenderer.material.color = darkColor;
+            }
+            else if (timer1 >= T_start && timer1 <= T_finish)
+            {
+                // Progress
+                blockrenderer.material.color = originalColor;
+            }
+            else if (timer1 > T_finish)
+            {
+
+                // move to Sink
                 currentPosition = transform.position;
                 blockrenderer.material.color = Color.black;
                 moveProgress = 0.0f;
                 targetPosition = sink;
-            }
-            
-            Sink();
-
-            isFinished = true;
-            return;
-        }
-        // Part 2
-        if (delta > target_delta) // 단 한 번 호출되는 함수. movetime 도달을 제어
-        {
-            delta = 0.0f;
-            // 0.5에 timer로 0.5를 감지해서 update
-            currentindex += 1; // 0에서 1로 변경
-            SetTarget(positions[currentindex]);
-            moveProgress = 0.0f;
-            colorChangeProgress = 0.0f;
-            colorChangeDuration = finishtime[currentindex] - starttime[currentindex];
-            blockrenderer.material.color = originalColor;
-
-            if (currentindex != num_process - 1) // 만약 마지막 process가 아니라면, 1 더해줌
-            {
-                target_delta = movetime[currentindex + 1] - movetime[currentindex];
+                Sink();
             }
             else
             {
-                target_delta = float.MaxValue;
+                // wait
             }
 
         }
 
-        if (timer >= movetime[currentindex] && timer <= starttime[currentindex])
-        {
-            blockrenderer.material.color = darkColor;
-            Move();
-        }
-        else
-        {
-            if (transform.position != targetPosition)
-            {
-                Move();
-            }
-            if(timer > starttime[currentindex] && timer < finishtime[currentindex])
-            {
-                check_arrival();
-                blockrenderer.material.color = originalColor;
-                //UpdateColorOverTime();
-            }
-            else
-            {
-                blockrenderer.material.color = darkColor;
-            }
-        }
-        
         // 타이머 증가
-        timer += Time.deltaTime;
-        delta += Time.deltaTime;
+        timer1 += Time.deltaTime;
+        // timer2 += Time.deltaTime;
         // Debug.Log("Time:"+timer+"| colorprogress: "+colorChangeProgress+ "| colorduration: " + colorChangeDuration);
         
     }
+
     void check_arrival()
     {
         if (transform.position == targetPosition)
@@ -220,14 +192,14 @@ public class Block : MonoBehaviour
     }
 
     // 시간이 지나면서 색상이 변하는 함수
-    void UpdateColorOverTime()
-    {
-        //Debug.Log("UpdateColorOverTime called");
-        colorChangeProgress += Time.deltaTime / colorChangeDuration;
+    //void UpdateColorOverTime()
+    //{
+    //    //Debug.Log("UpdateColorOverTime called");
+    //    colorChangeProgress += Time.deltaTime / colorChangeDuration;
 
-        // 색상 변화 진행 상태에 따라 색상 보간 (Lerp 사용)
-        Color currentColor = Color.Lerp(originalColor, processColor, colorChangeProgress);
+    //    // 색상 변화 진행 상태에 따라 색상 보간 (Lerp 사용)
+    //    Color currentColor = Color.Lerp(originalColor, processColor, colorChangeProgress);
         
-        blockrenderer.material.color = currentColor;
-    }
+    //    blockrenderer.material.color = currentColor;
+    //}
 }
--- Machine-Shop/Assets/Scenes/GameManager.cs
+++ Machine-Shop/Assets/Scenes/GameManager.cs
@@ -3,27 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.IO;  // for File I/O
 using System.Globalization;  // for converting strings to floats
-
+using System.Linq;
+using System;
 
 class Job
 {
     public Job(Color _color, int _blockindex, GameObject prefab, 
-        List<Vector3> _positions, Vector3 _source, Vector3 _sink, 
-        float[] _movetime, float[] _finishtime, float[] _starttime)
+        Vector3 _position, Vector3 _source, Vector3 _sink, 
+        float[] _timetable)
     {
         
         // Instantiate는 static 메서드이므로 Object.Instantiate로 호출해야 합니다.
-        block = Object.Instantiate(prefab);
+        block = UnityEngine.Object.Instantiate(prefab);
         // 생성한 인스턴스에서 Block 컴포넌트 가져오기
         Block blockComp = block.GetComponent<Block>();
         // 초기화 메서드 호출
-        blockComp.Initialize(_color, _blockindex, 
-            _positions, _source, _sink,
-            _movetime, _finishtime, _starttime);
+        blockComp.Initialize(_color, _blockindex, _source, _position, _sink, _timetable);
+        Debug.Log("Job" + _blockindex + " Created!");
+    }
 
-        // 활성화
+    public void Activate()
+    {
         block.SetActive(true);
-
     }
 
     public GameObject block;
@@ -35,7 +36,7 @@ class Machine
     {
 
         // Instantiate는 static 메서드이므로 Object.Instantiate로 호출해야 합니다.
-        process = Object.Instantiate(prefab);
+        process = UnityEngine.Object.Instantiate(prefab);
         // 생성한 인스턴스에서 Block 컴포넌트 가져오기
         Process processComp = process.GetComponent<Process>();
         processComp.transform.position = initialPosition;
@@ -57,13 +58,16 @@ public class GameManager : MonoBehaviour
 
     public string colorPath = "color.csv"; // CSV 파일 경로 (프로젝트 폴더 내)
     public string positionPath = "position.csv";
+    public string timePath = "time.csv";
     private List<Color> colorData; // CSV 파일로부터 읽은 RGB 값들 저장
     private List<Vector3> positionData;
+    private List<Tuple<int, int, float, float, float, float, float>> timeData;
     public Transform parent;
-    static int numBlocks = 15;
-    static int numProcesses = 10;
+    static int numBlocks = 100;
+    static int numMachine = 5;
+    static int numProcesses = 1;
 
-    public bool isFinished;
+    public bool isfinished;
 
     // Job 객체 배열 선언
     private Job[] jobs;  // Job[] 타입으로 선언
@@ -74,120 +78,75 @@ public class GameManager : MonoBehaviour
     int count;
     List<Vector3> source;
     List<Vector3> sink;
-
-
+    private float timer = 0.0f;
+    int num_created;
     // Start is called before the first frame update
     void Start()
     {
-        Time.timeScale = 2f;
+        Time.timeScale = 3f;
         // 1. CSV 파일 읽기
         colorData = ReadColorsFromCSV(colorPath);
         positionData = ReadPositionFromCSV(positionPath);
-
+        timeData = ReadTimeTableFromCSV(timePath);
+        num_created = 0;
 
         // Job 배열 크기 지정
         jobs = new Job[numBlocks];  // 3개의 Job을 담을 수 있는 배열 생성
-        machines = new Machine[numProcesses];
+        machines = new Machine[numMachine];
 
-        source = stackPositions(5, 0.0f, 0.0f, 1.5f, 0.6f, -0.6f);
-        sink = stackPositions(5, -2.0f, 0.0f, -2.0f, -0.6f, 0.6f);
-
-        List<float[]> move = new List<float[]>();
-        List<float[]> start = new List<float[]>();
-        List<float[]> finish = new List<float[]>();
-        float interval = 2.0f;
-        List<float> t = new List<float>();
-        for (int u = 0; u < numProcesses; u++)
-        {
-            t.Add(interval * u);
-        }
-
-        for (int v = 0; v< numBlocks; v++)
-        {
-            // 새로운 배열을 생성 (첫 번째 요소는 0.0f, 나머지는 t[i] + 0.5f)
-            float[] _move = new float[1+numProcesses];
-            float[] _start = new float[1+numProcesses];
-            float[] _finish = new float[1+numProcesses];
-            _move[0] = 0.0f;  // 첫 번째 값은 0.0f
-            _start[0] = 0.0f;  // 첫 번째 값은 0.0f
-            _finish[0] = 0.0f;  // 첫 번째 값은 0.0f
+        source = stackPositions(10, 0.0f, 0.0f, 6.0f, 0.6f, -0.6f);
+        sink = stackPositions(10, 16.0f, 0.0f, 2.0f, -0.6f, -0.6f);
         
-            for (int i = 0; i < numProcesses; i++)
-            {
-                _move[i + 1] = t[i];
-                _start[i + 1] = t[i] + 0.5f;  // t[i] + 0.5f를 result의 두 번째부터 채움
-            }
-
-            move.Add(_move);
-            start.Add(_start);
-            List<float> rnd = new List<float>();
-            for (int u = 0; u < numProcesses; u++)
-            {
-                rnd.Add(Random.Range(1.0f, 1.5f));
-            }
-            for (int i = 0; i < numProcesses; i++)
-            {
-                _finish[i + 1] = t[i] + rnd[i];
-            }
-            finish.Add(_finish);
-            for (int u = 0; u < numProcesses; u++)
-            {
-                t[u] += interval;
-            }
-            
-        }
-            
         // 각각의 Job 객체 생성
         for (int d = 0; d < numBlocks; d++)
         {
             int coloridx = d % colorData.Count;
+            int machineidx = d % numMachine;
+            Debug.Log("-------------------------------------------");
+            Debug.Log(d);
+            Debug.Log("Machine: "+d);
+            Debug.Log(timeData[d]);
+            Debug.Log(positionData[timeData[d].Item2]);
+
+            float[] timetable = new float[] { timeData[d].Item3, timeData[d].Item4, timeData[d].Item5, timeData[d].Item6, timeData[d].Item7 };
             jobs[d] = new Job(colorData[coloridx], d, BlockPrefab,
-            positionData, source[d], sink[d],
-            move[d], finish[d], start[d]);
+            positionData[timeData[d].Item2], source[d], sink[d], timetable);
         }
 
             
-
-        ////------------------------------------------------
-        //float[] move2 = new float[] { 0.0f, 4.0f, 7.0f, 9.0f,};
-        //float[] start2 = new float[] { 0.0f, 4.5f, 7.5f, 9.5f};
-        //float[] finish2 = new float[] { 0.0f, 7.0f, 9.0f, 11.0f};
-        //jobs[1] = new Job(colorData[1], 1, BlockPrefab,
-        //    positionData, source[1], sink[1],
-        //    move2, finish2, start2);
-        ////------------------------------------------------
-        
-        //float[] move3 = new float[] { 0.0f, 7.0f, 9.0f, 11.0f};
-        //float[] start3 = new float[] { 0.0f, 7.5f, 9.5f, 11.5f};
-        //float[] finish3 = new float[] { 0.0f, 8.0f, 10.0f, 13.0f};
-
-        //jobs[2] = new Job(colorData[2], 2, BlockPrefab, 
-        //    positionData, source[2], sink[2],
-        //    move3, finish3, start3);
-
-        //------------------------------------------------
-        for (int j = 0; j < numProcesses; j++)
+        for (int j = 0; j < numMachine; j++)
         {
             Debug.Log(j);
-            machineposition = new Vector3(positionData[j+1].x, -0.31f, positionData[j+1].z);
+            machineposition = new Vector3(positionData[j].x, -0.305f, positionData[j].z);
             machines[j] = new Machine(ProcessPrefab, machineposition);
             Debug.Log("New machine " + j + " generated on " + machineposition);
         }
-
-
-
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
-        check_termination();
+        isfinished = check_termination();
+        if (isfinished)
+        {
+            return;
+        }
+
+        if (timer >= timeData[num_created].Item3)
+        {
+            jobs[num_created].Activate();
+            if (num_created <numBlocks - 1)
+            {
+                num_created++;
+            }
+        }
+        timer += Time.deltaTime;
 
     }
 
-    void check_termination()
+    bool check_termination()
     {
 
         int count = 0;
@@ -202,14 +161,53 @@ public class GameManager : MonoBehaviour
         if (count == jobs.Length)
         {
             Debug.Log("All Jobs Finished!");
+            return true;
         }
+        else
+        {
+            return false;
+        }
+    }
+    List<Tuple<int, int, float, float, float, float, float>> ReadTimeTableFromCSV(string file)
+    {
+        List<Tuple<int, int, float, float, float, float, float>> timeCriteria = new List<Tuple<int, int, float, float, float, float, float>>();
+
+        string path = Path.Combine(Application.dataPath, file);  // 파일 경로
+
+        if (File.Exists(path))
+        {
+            string[] lines = File.ReadAllLines(path);  // 모든 라인을 읽음
+
+            foreach (string line in lines.Skip(1))
+            {
+                string[] values = line.Split(',');  // 쉼표로 값들을 분리
+                int idx = int.Parse(values[0], CultureInfo.InvariantCulture); 
+                int machine = int.Parse(values[1], CultureInfo.InvariantCulture); 
+                float release = float.Parse(values[2], CultureInfo.InvariantCulture); 
+                float move = float.Parse(values[3], CultureInfo.InvariantCulture);  
+                // float setup = float.Parse(values[4], CultureInfo.InvariantCulture);
+                float setup = float.Parse(values[3], CultureInfo.InvariantCulture) + 2.0f;
+                float start = float.Parse(values[5], CultureInfo.InvariantCulture); 
+                float finish = float.Parse(values[6], CultureInfo.InvariantCulture);  
+
+                // Tuple을 리스트에 추가
+                timeCriteria.Add(Tuple.Create(idx, machine, release, move, setup, start, finish));
+            }
+
+        }
+        else
+        {
+            Debug.LogError("CSV file not found at: " + path);
+        }
+
+        return timeCriteria;
+
     }
     // CSV 파일에서 RGB 값을 읽어와서 List<float[3]> 형태로 저장
     List<Vector3> ReadPositionFromCSV(string file)
     {
         List<Vector3> positions = new List<Vector3>();  // 색상 리스트
         string path = Path.Combine(Application.dataPath, file);  // 파일 경로
-        positions.Add(new Vector3(0.0f, 0.0f, 0.0f));  // float[3]로 저장
         if (File.Exists(path))
         {
             string[] lines = File.ReadAllLines(path);  // 모든 라인을 읽음
